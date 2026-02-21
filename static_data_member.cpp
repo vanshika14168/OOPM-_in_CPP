9a0c2785a9bc2a7a8f0c82db8364a2cc1909bdd8
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-02-21 15:46:45 +0530

    Add files via upload

---
+++ static_data_member.cpp
@@ -0,0 +1,57 @@
+//static data member is same as all the object of class
+//they are attribute of class
+//they are same as all object
+
+//STATIC DATA MEMBER
+#include<iostream>
+using namespace std;
+/*class customer{
+    public:
+    string name;
+    int acc_no;
+    int balance;
+    static int total_customer;
+
+    public:
+    customer(string name,int acc_no,int balance){
+        this->name=name;
+        this->acc_no=acc_no;
+        this->balance=balance;
+        total_customer++;
+    }
+
+    void display(){
+        cout<<"name is:"<<name<<endl;
+        cout<<"account number is:"<<acc_no<<endl;
+        cout<<"balance is:"<<balance<<endl;
+        
+        
+    }
+
+    void display_total(){
+        cout<<"total customer:"<<total_customer<<endl;
+    }
+
+};
+
+//inilatization of static member 
+    //data_type_of_static_member class_name ::total_customer(static member name)=0
+    //int customer :: total_customer=0
+    int customer :: total_customer=0;
+
+
+int main(){
+
+    customer A("vanshi",2343,1000);
+    //customer A1("khushi",8966,10000);
+    A.display();
+    A.display_total();
+    customer A1("khushi",8966,10000);
+    A1.display();
+    A1.display_total();
+    
+    
+    return 0;
+}*/
+
+
