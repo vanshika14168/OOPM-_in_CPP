7b18a7e65d12a780bcbb0e9fbcbd340a9b286a64
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-02-26 10:07:30 +0530

    constructor

---
+++ constructor.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+/*class customer{
+    public:
+    string name;
+
+    customer(){//default constructor
+        //constructor is same as class name
+        cout<<"constructor is called:"<<endl;
+    }
+};
+int main(){
+
+    customer A;
+    //constructor is automaticallt called when objet is created
+    
+    return 0;
+}*/
+
+//PARAMETRIZED CONSTRUCTOR
+#include<iostream>
+using namespace std;
+/*class customer{
+    public:
+    string c_name;
+    int age;
+
+    public:
+    customer(string s,int a){
+        c_name=s;
+        age=a;
+        cout<<"customer name is:"<<c_name<<endl;
+        cout<<"customer age is:"<<age<<endl;
+    }
+};
+int main(){
+
+    customer A("vanshika",19);
+
+    return 0;
+}*/
+
+
+//THIS KEYWORD
+#include<iostream>
+using namespace std;
+
+/*class customer{
+    public:
+    int age;
+    int roll_no;
+    string name;
+    char grade;
+
+    public:
+    customer(string name,int age,int roll_no,char grade){
+        this->name=name;
+        this->age=age;
+        this->roll_no=roll_no;
+        this->grade=grade;
+        cout<<"name is:"<<name<<endl;
+        cout<<"age is:"<<age<<endl;
+        cout<<"roll_no is:"<<roll_no<<endl;
+        cout<<"grade is:"<<grade<<endl;
+    }
+};
+int main(){
+
+    customer A("vanshika",19,74,'A');
+
+    return 0;
+}*/
